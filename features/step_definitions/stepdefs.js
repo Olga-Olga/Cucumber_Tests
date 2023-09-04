54727d00fdc386f8f2bedadb830e7dafdee33d62
Olga-Olga <olga_l@ukr.net>
2023-09-04 18:25:50 +0300

    second2

--- features/step_definitions/stepdefs.js
+++ features/step_definitions/stepdefs.js
@@ -2,14 +2,6 @@ const assert = require("assert");
 const { Given, When, Then } = require("@cucumber/cucumber");
 
 function isItFriday(today) {
-  //   if (today === "Friday") {
-  //     return "TGIF";
-  //   }
-  //   //skip this step
-  //   else {
-  //     return "Nope";
-  //     }
-
   return today === "Friday" ? "TGIF" : "Nope";
 }
 
@@ -18,16 +10,6 @@ Given("today is the first summer month", function () {
   console.log("Yes, it's the first summer month");
   //   return "pending";
 });
-
-// Given("today is Sunday", function () {
-//   this.today = "Sunday"; // Обращение к общему объекту, предоставляемому Cucumber
-//   //   return "pending";
-// });
-
-// Given("today is Friday", function () {
-//   this.today = "Friday"; // Обращение к общему объекту, предоставляемому Cucumber
-//   //   return "pending";
-// });
 
 Given("today is {string}", function (givenDay) {
   this.today = givenDay; // Обращение к общему объекту, предоставляемому Cucumber
